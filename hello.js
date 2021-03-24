60b0f83cb1b6f3167259192bed102235451c93a8
MikaelAbyss <65512888+MikaelAbyss@users.noreply.github.com>
2021-03-23 21:23:16 -0300

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,10 @@
+function hi () {
+    let weight = prompt();
+            if (weight%2==0 && weight != 2) {
+                console.log("Yes");
+            } else {
+                console.log("N");
+            }
+        rl.close();
+    };
+hi();
